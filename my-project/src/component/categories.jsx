750392ea59ec71dc519b1151112e2a643a719575
subomiotusanya <otusanya.olasubomi@stu.cu.edu.ng>
2024-08-14 14:23:54 +0100

    changes

---
+++ my-project/src/component/categories.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./categories.css"
+
+const Categories = () => {
+  return(
+    <>
+    <div className="cart">
+      <h5>
+        Categories
+      </h5>
+    </div>
+    </>
+  )
+}
+
+export default Categories;
