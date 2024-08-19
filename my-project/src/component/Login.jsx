ef546efc0b0cd2c4bf04242e96e34914ed21c02c
KOD04 <david.oshiyemi@stu.cu.edu.ng>
2024-08-19 09:41:29 +0100

    changes

--- my-project/src/component/Login.jsx
+++ my-project/src/component/Login.jsx
@@ -1,40 +1,37 @@
 import "../component/login.css"
 import LoginPic from "../assets/Login.png"
 import { Navigate, useNavigate } from "react-router-dom"
-import "./login.css"
+import Logo from "../assets/LOGO.png"
 
 const Login = () => {
   const navigate = useNavigate()
 
   return (
     <>
-    <div className="container">
-        <div className="img">
-          <p className="text">Welcome to ChicCloset</p>
-          <p className="text1">Discover a world of fashion tailored just for you</p>
+    <div className="containers">
+        <div className="imgs">
+          <p className="texting">Welcome to ChicCloset</p>
+          <p className="texting1">Discover a world of fashion tailored just for you</p>
           <img src={LoginPic} className="image1" />
         </div>
-        <div className="content">
-          <div className="logo">
-            <div className="normal">C</div>
-            <div className="updown">C</div>
-          </div>
-          <div className="text3">Login</div>
-          <div className="text4">Welcome back!</div>
+        <div className="contents">
+            <img src={Logo} alt="chiccloset logo" className="imaging"/>
+          <div className="tmsg3">Login</div>
+          <div className="tmsg4">Welcome back!</div>
           <div className="googles"><div className="flat-color-icons--google"></div>Sign in with google</div>
 
-          <div className="or"><hr/>or<hr/></div>
+          <div className="orline"><hr/>or<hr/></div>
 
-          <div className="form">
-            <p className="text5">Username</p>
+          <div className="forming">
+            <p className="tmsg5">Username</p>
             <input type="text" id="fullnames" placeholder="jeffdan@gmail.com" />
           </div>
-          <div className="form">
-            <p className="text5">Password</p>
+          <div className="forming">
+            <p className="tmsg5">Password</p>
             <input type="password" id="fullnames" placeholder="Password" />
           </div>
           <button type="submit" id="continues">Login</button>
-          <div className="login">Don't have an account? <a onClick={()=> navigate("/Buyven")}>Sign up</a></div>
+          <div className="logins">Don't have an account? <a onClick={()=> navigate("/Buyven")}>Sign up</a></div>
         </div>
       </div>
     </>
