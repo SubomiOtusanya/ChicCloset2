265a89607abd433d0f62c5743ee9cce046edc75a
KOD04 <david.oshiyemi@stu.cu.edu.ng>
2024-08-21 17:06:04 +0100

    changes

--- my-project/src/component/Product/Sidebar.jsx
+++ my-project/src/component/Product/Sidebar.jsx
@@ -1,10 +1,28 @@
 import LOGO from "../../assets/Frame 4.png"
 import "./Sidebar.css"
-function Sidebar(){
+function Sidebar({username}){
     return(
         <div className="sidepiece">
-            <img src={LOGO} className="logoo"/>
-            <p></p>
+                <img src={LOGO} className="logoo"/>
+            <div className="acting">
+                <div className="actions">
+                    <a className="activ"><div className="mynaui--home "></div>Home</a>
+                    <a className="activ"><div className="bx--category"></div>Catergories</a>
+                    <a href="#" className="activ"><div  className="gg--shopping-cart"></div>My Cart</a>
+                    <a href="#" className="activ"><div className="wi--time-4"></div>Order History</a>
+                </div>
+                <div className="actions">
+                    <a href="#" className="activ"><div className="lets-icons--setting-line"></div>Settings</a>
+                    <a href="#" className="activ"><div className="lucide--messages-square"></div>Support</a>
+                    <div className="profiling">
+                        <div className="circle"></div>
+                        <div className="prof">
+                            <p>Profile</p>
+                            <p>{username}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
         
     )
